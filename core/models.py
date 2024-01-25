d6d80bf8801b1f902ac9edfbc072607974c79ea1
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2024-01-25 15:16:39 +0000

    first model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Plate(models.Model):
+    name = models.CharField(max_length = 20)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
+
+class Check(models.Model):
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    plate = models.ForeignKey(Plate, on_delete= models.PROTECT, related_name= "checks")
+    date = models.DateTimeField()
+    message = models.CharField()
+    category = models.ForeignKey(Category, on_delete= models.SET_NULL, related_name = "checks")
+
+    def __str__(self):
+        return f"{self.amount} {self.Plate.name} {self.Category.name}"
