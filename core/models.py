69c30c036784a8339a0a2d45d72131ecf4763c9f
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2024-01-26 04:07:38 +0000

    first github migrations

--- core/models.py
+++ core/models.py
@@ -18,8 +18,8 @@ class Check(models.Model):
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     plate = models.ForeignKey(Plate, on_delete= models.PROTECT, related_name= "checks")
     date = models.DateTimeField()
-    message = models.CharField()
-    category = models.ForeignKey(Category, on_delete= models.SET_NULL, related_name = "checks")
+    message = models.CharField(max_length = 20)
+    category = models.ForeignKey(Category, on_delete= models.SET_NULL, null = True, blank = True, related_name = "checks")
 
     def __str__(self):
         return f"{self.amount} {self.Plate.name} {self.Category.name}"
